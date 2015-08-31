864477167a05cf707a744259f4bb3217c7c03637
takunoko <takunoko@gmail.com>
2015-08-31 09:11:51 +0900

    ヘッダーファイルにある関数を形だけ生成

--- event.c
+++ event.c
@@ -6,3 +6,51 @@
 void e_sea(PLAYER *player_list, int p_num){
 
 }
+
+void e_sea(PLAYER *,int){
+
+}
+
+void e_bbq(PLAYER *,int){
+
+}
+
+void e_mountain(PLAYER *,int){
+
+}
+
+void e_karaoke(PLAYER *,int){
+
+}
+
+void e_swimming(PLAYER *,int){
+
+}
+
+void e_swimming(PLAYER *,int){
+
+}
+
+void e_study(PLAYER *,int){
+
+}
+
+void e_bowling(PLAYER *,int){
+
+}
+
+void e_game(PLAYER *,int){
+
+}
+
+void e_game(PLAYER *,int){
+
+}
+
+void e_drive(PLAYER *,int){
+
+}
+
+void e_shopping(PLAYER *,int){
+
+}
--- tools.c
+++ tools.c
@@ -1,9 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 #include "data_set.h"
 
-void init_program(){
+// プログラム本体の初期化
+void init_program(DAY, int){
+    // 乱数初期化
+    srand((unsigned)time(NULL));
+
 }
 
+// ダイスを振る
 int throw_dice(int max_num){
-	int num = 3;
+    num = rand()%max_num;
 	return num;
+}
+
+// ゲームの初期化
+void init_game(PLAYER){
+}
+
+// ダイスを振る
+int throw_dice(int){
+}
+
+// プレイヤーを動かす
+void move_player(PLAYER){
+}
+
+// イベント発生時にどうにかする
+void switch_event(PLAYER, int){
 }
